b802986f98c70c0d118963f813105e60f4002dfd
Bruno de Jesus Santos <bru_blink@hotmail.com>
2018-03-21 16:39:24 -0300

    Create the structe

---
+++ WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using PusherServer;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private string app_id = "495978";
+        private string key = "a5935c2b82279378ad4d";
+        private string secret = "865ca4c795048d3d321c";
+        private PusherOptions options = new PusherOptions
+        {
+            Cluster = "us2",
+            Encrypted = true
+        };
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult List()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(User user)
+        {
+            var pusher = new Pusher(
+              app_id,
+              key,
+              secret,
+              options);
+
+            var result = await pusher.TriggerAsync(
+              "chat",
+              "conversation",
+              new { User = user });
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
